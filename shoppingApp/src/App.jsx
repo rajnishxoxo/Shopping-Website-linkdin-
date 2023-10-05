d7d641df63b2bbd1cde4d7c6725e0184ec6452e6
Rajnish <srajnish235.rs@gmail.com>
2023-10-05 19:02:45 +0530

    Installed MUI Icons

--- shoppingApp/src/App.jsx
+++ shoppingApp/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import LoginIcon from '@mui/icons-material/Login';
+
 
 const App = () => {
-  return <div></div>;
+  return <div>
+    <h1 className="text-4xl">Hello</h1>
+    <LoginIcon/>
+  </div>;
 };
 
 export default App;
