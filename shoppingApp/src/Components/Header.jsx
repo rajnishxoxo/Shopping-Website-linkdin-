e92990e1d843e9fe5d01dee722ed669207296a55
Rajnish <srajnish235.rs@gmail.com>
2023-10-13 16:23:39 +0530

    Update Header.jsx

--- shoppingApp/src/Components/Header.jsx
+++ shoppingApp/src/Components/Header.jsx
@@ -5,9 +5,21 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import LoginIcon from "@mui/icons-material/Login";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../Utils/firebase";
 const Header = () => {
   const cart = useSelector((state) => state.cart.item);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setUserLoggedIn(true);
+      // ...
+    } else {
+      // User is signed out
+      // ...
+    }
+  });
 
   return (
     <div className="flex flex-col w-full mt-2 justify-evenly shadow-lg h-auto items-center lg:flex-row lg:h-20">
@@ -30,9 +42,15 @@ const Header = () => {
             WishList <FavoriteIcon />
           </li>
           <Link to={"/login"}>
-            <li class="text-2xl mb-2 font-medium relative">
-              Login <LoginIcon />
-            </li>
+            {userLoggedIn ? (
+              <li className="text-2xl mb-2 font-medium relative">
+                Logout <LoginIcon />
+              </li>
+            ) : (
+              <li className="text-2xl mb-2 font-medium relative">
+                Login <LoginIcon />
+              </li>
+            )}
           </Link>
         </ul>
       </div>
